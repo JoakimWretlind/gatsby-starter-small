749b5d54fa9e5f352a0fe7766bcfca9774824cbf
JoakimWretlind <joakim.wretlind@gmail.com>
2021-06-25 16:44:23 +0200

    Added gatsby-plugin-react-helmet And gastby-source-filesystem

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,18 @@
 module.exports = {
+  siteMetadata: {
+    title: `Gatsby Boilerplate`,
+    description: `Boilerplate to get your Gatsby up and running inno time.`,
+    author: `Joakim Wretlind`,
+  },
   plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `assets`,
+        path: `${__dirname}/src/assets`,
+      },
+    },
     `gatsby-plugin-styled-components`
   ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <>
       <GlobalStyle />
-      <h1>Hello World</h1>
+      <h1>Hello There</h1>
     </>
   )
 
