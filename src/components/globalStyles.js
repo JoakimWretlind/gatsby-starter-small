9ffac8c13e7cce9bce654fa28c66afb14b3f318c
Joakim Wretlind <joakim.wretlind@gmail.com>
2021-06-26 12:09:56 +0200

    small startup for gatsby

--- src/components/globalStyles.js
+++ src/components/globalStyles.js
@@ -7,19 +7,12 @@ export const GlobalStyle = createGlobalStyle`
     margin: 0;
     padding: 0;
     box-sizing: inherit;
+    text-decoration: none;
   }
-  
-  html {
-    font-size: 62.5%;
-  }
-  
+    
   body {
     box-sizing: border-box;
     font-family: 'lato', sans-serif;
-  }
-  
-  a {
-    text-decoration: none;
   }
   
   ul, li {
